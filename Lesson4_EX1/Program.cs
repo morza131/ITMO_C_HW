6a838d98a70c031762d0542c57db15a4c16a677a
morza131 <morza131@gmail.com>
2023-06-21 16:59:10 +0300

    Добавлено выполнение заданий урока №4

---
+++ Lesson4_EX1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4_EX1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sqr=0;
+            for (int i = 1; i < (2*n); )
+            {
+                sqr = sqr + i;
+                i += 2;
+                Console.WriteLine(sqr);
+            }
+        }
+    }
+}
---
+++ Lesson4_EX2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4_EX2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int countPositive = 0;
+            int countNegative = 0;
+            Console.WriteLine("вводите числа последовательности, 0 - конец последовательности");
+            do
+            {
+                a = Convert.ToInt32(Console.ReadLine());
+                if (a>0)
+                    countPositive++;
+                else if (a<0)
+                    countNegative++;
+            }
+            while (a != 0);
+            if (countPositive > countNegative)
+            {
+                Console.WriteLine("Положительных чисел больше, чем отрицательных");
+            }
+            else if (countPositive == countNegative)
+            {
+                Console.WriteLine("Положительных чисел столько же, сколько отрицательных");
+            }
+            else
+                Console.WriteLine("Отрицательных чисел больше, чем положительных");
+        }
+    }
+}
