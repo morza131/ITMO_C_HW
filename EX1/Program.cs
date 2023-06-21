5961b3ca19512a1d51dff33cd09f0e0caf103d03
morza131 <morza131@gmail.com>
2023-06-21 15:42:56 +0300

    Добавьте файлы проекта.

---
+++ EX1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты 3 точек прямоугольника");
+            Console.WriteLine("x1=");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y1=");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("x2=");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y2=");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("x3=");
+            int x3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y3=");
+            int y3 = Convert.ToInt32(Console.ReadLine());  
+            if ((x1 == x2 ||  x2 == x3 || x1 ==x3)&& (y1 == y2 || y2 == y3 || y1 == y3)) 
+            {
+                int x4 = (x1 == x2) ? x3 : (x2 == x3) ? x1 : x2;
+                int y4 = (y1 == y2) ? y3 : (y2 == y3) ? y1 : y2;
+                Console.WriteLine("x4 = {0}, y4 = {1}",x4,y4);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Координаты введены с ошибкой");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ EX3/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите возраст");
+            byte age = Convert.ToByte(Console.ReadLine());
+            string first="";
+            string second="";
+            string third = "";
+            if (age > 19 && age < 30)
+                first = "двадцать ";
+            else if (age < 40)
+                first = "тридцать ";
+            else if (age < 50)
+                first = "сорок ";
+            else if (age < 60)
+                first = "пятьдесят ";
+            else if (age < 70)
+                first = "шестьдесят ";
+            else
+                Console.WriteLine("некорректный возраст");
+
+            switch (age % 10)
+            {
+                case 0:
+                    second = "";
+                    break;
+                case 1:
+                    second = "один ";
+                    break;
+                case 2:
+                    second = "два ";
+                    break;
+                case 3:
+                    second = "три ";
+                    break;
+                case 4:
+                    second = "четыре ";
+                    break;
+                case 5:
+                    second = "пять ";
+                    break;
+                case 6:
+                    second = "шесть ";
+                    break;
+                case 7:
+                    second = "семь ";
+                    break;
+                case 8:
+                    second = "восемь ";
+                    break;
+                case 9:
+                    second = "девять ";
+                    break;               
+            }
+
+            if (age % 10 == 0 || age % 10 > 4)
+                third = "лет";
+            else if (age % 10 == 1)
+                third = "год";
+            else
+                third = "года";
+
+            Console.WriteLine(first +second+ third);
+        }
+    }
+}
