c6f3147e60d196b36bf3ded6b6b179f61cceddb9
morza131 <morza131@gmail.com>
2023-06-23 13:21:32 +0300

    Выполнены упражнения урока 6

---
+++ Lesson6_EX1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6_EX1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string str = Console.ReadLine();            
+            string[] strArr = str.Split(' ');
+            int k = 0;
+            foreach (string word in strArr)
+            {
+                if (word.Length > k)
+                    k = word.Length;
+            }
+            Console.WriteLine("Самое длинное слово содержит {0} символов",k);
+        }
+    }
+}
---
+++ Lesson6_EX2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6_EX2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string str = Console.ReadLine();
+            string[] strArr = str.ToLower().Split(' ');
+            string newstr="";
+            foreach (var word in strArr)
+            {
+                newstr+=word;
+            }
+            Console.WriteLine(newstr);
+            bool flag = true;
+            for (int i = 0; i < newstr.Length/2; i++)
+            {
+                char ch = newstr[i];
+                Console.WriteLine(ch);
+                if (!(newstr[i] == newstr[newstr.Length-i-1])) 
+                {
+                    flag = false;
+                }
+            }
+            Console.WriteLine(flag);
+        }
+    }
+}
