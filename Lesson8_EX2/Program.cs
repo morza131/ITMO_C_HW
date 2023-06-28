169c0f81a2b6e72e31ff96d5c26fd5c19ba5caca
morza131 <morza131@gmail.com>
2023-06-28 15:32:46 +0300

    добавлено решение ДЗ 8 урока

---
+++ Lesson8_EX2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson8_EX2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\L8EX2.txt";
+            Random random = new Random();
+            using(StreamWriter sw = new StreamWriter(path)) 
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(random.Next(-10,10)) ;
+                }
+            }
+            int a = 0;
+            using(StreamReader sr = new StreamReader(path)) 
+            {
+            for (int i = 0; i<10; i++)
+                {
+                    a += Convert.ToInt32(sr.ReadLine());
+                }
+            }
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson8_EX3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson8_EX3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\L8EX3.txt";
+            string[] words;
+            string[] strings;
+            int symbols = 0;
+            string text = null;
+            using(StreamReader sr = new StreamReader(path)) 
+            {
+                text = sr.ReadToEnd();              
+            }
+            symbols = text.Count();
+            strings = text.Split('\n');
+            text.Trim(',', ' ', '.', '\n');
+            words = text.Split(' ');
+            Console.WriteLine("Символов {0}, слов {1}, строк {2}", symbols, words.Length, strings.Length);
+            Console.ReadKey();            
+        }
+    }
+}
