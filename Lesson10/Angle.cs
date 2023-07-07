d0ea237d78aa6229e6495ae2c6a79845161af6f4
morza131 <morza131@gmail.com>
2023-07-07 14:30:41 +0300

    Добавил задание урока 10

---
+++ Lesson10/Angle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10
+{
+    class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+
+      public int Gradus
+        { get { return gradus; } set {  gradus = value; } }
+        public int Min
+        { get { return min; } set {  min = value %60; Gradus += value / 60; } }
+        public int Sec
+        { get { return sec; } set {  sec = value %60; Min += value / 60; } }
+        public Angle (int gradus, int min, int sec)
+        {
+            Gradus = gradus;
+            Min = min;
+            Sec = sec;
+        }
+
+       
+        public double ToRadians()
+        {
+            double rads =(gradus + min + sec)* Math.PI / 180;
+            return rads;
+        }
+    }
+}
---
+++ Lesson10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите по очереди градус, минуты, секунды");
+            int gradus = Convert.ToInt32(Console.ReadLine());
+            int min = Convert.ToInt32(Console.ReadLine());
+            int sec = Convert.ToInt32(Console.ReadLine());
+                       
+            Angle angle = new Angle(gradus, min, sec);
+            double rad = angle.ToRadians();
+            Console.WriteLine(angle.Gradus);
+            Console.WriteLine(rad);
+            Console.ReadKey();
+        }
+    }
+}
