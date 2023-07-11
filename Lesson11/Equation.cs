ceaff501125b60672770eef6c608d28ac309d0d1
morza131 <morza131@gmail.com>
2023-07-11 16:47:27 +0300

    Добавлено решение урока 11

---
+++ Lesson11/Equation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson11
+{
+    public struct Equation
+    {
+        double k;
+        double b;
+        
+        public Equation(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public double Root()
+        {
+            return -b / k;
+        }
+    }
+}
---
+++ Lesson11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите К и В");
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            Equation equation = new Equation(k,b);
+            double x = equation.Root();
+            if (double.IsInfinity(x))
+                Console.WriteLine("Ошибка деления на 0");
+            else
+                Console.WriteLine($"x = {x}");
+            
+            
+        }
+    }
+}
