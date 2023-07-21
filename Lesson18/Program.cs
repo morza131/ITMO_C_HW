81c4291598d5ad6aba021c58fff736f9536f1819
morza131 <morza131@gmail.com>
2023-07-21 12:57:28 +0300

    Выполнено задание урока 18

---
+++ Lesson18/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string teststring1 = "{}{][[]][]{()())}";
+            string teststring2 = "{}[]()(({}))(";
+            string teststring3 = "{}[]()(({}))";
+            Stack<char> stack = new Stack<char>();
+            bool flag = false;
+            foreach (char i in teststring2) 
+            {
+                switch (i)
+                {
+                    case '(':
+                        stack.Push(')');
+                        break;
+                    case '[':
+                        stack.Push(']');
+                        break;
+                    case '{':
+                        stack.Push('}');
+                        break;
+                    default:
+                        if (stack.First() == i)
+                            stack.Pop();
+                        else
+                            flag = true;    
+                            break;
+                }
+                Console.WriteLine(stack.Count);
+                if (flag)
+                {
+                    Console.WriteLine("Строка неправильная!");
+                    Environment.Exit(0);
+                    break;                    
+                }
+            }
+            Console.WriteLine(stack.Count);
+            if (stack.Count == 0)
+                Console.WriteLine("Строка правильная!");
+            else
+                Console.WriteLine("Строка неправильная!");
+        }
+    }
+}
