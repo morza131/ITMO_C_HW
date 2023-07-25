fdc4b54605def42842fc76270f9f87ff01f287ad
morza131 <morza131@gmail.com>
2023-07-25 09:25:42 +0300

    Выполнено задание урока 20

---
+++ Lesson20/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson20
+{
+    internal class Program
+    {
+        public delegate double MyDelegate(double r);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус");
+            double r = Convert.ToDouble(Console.ReadLine());
+            MyDelegate myDelegate = Сircumference;
+            Console.WriteLine($"Длина окружности равна { myDelegate(r)}");
+            myDelegate = Square;
+            Console.WriteLine($"Площадь круга равна { myDelegate(r)}");
+            myDelegate = Volume;
+            Console.WriteLine($"Объем сферы равен { myDelegate(r)}");
+
+
+        }
+        public static double Сircumference(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        public static double Square (double r)
+        {
+            return Math.PI * r*r;
+        }
+        public static double Volume (double r) 
+        {
+            return Math.PI * r*r*r*4/3;
+        }
+    }
+}
